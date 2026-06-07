1b742f42664729093a9b972f3cedab66c42987c9
Efrain Serrano <eri3l2214@gmail.com>
2026-06-06 20:18:55 -0600

    Definimos tipos de parámetros para la navegación con TypeScript
    - Se creó el archivo de tipado en src/types/navigation.ts.
    
    - Se definieron los tipados RootStackParamList para el flujo de login y HomeTabParamList para las pestañas de la bitácora.

---
+++ HardwareCheck/src/types/navigation.ts
@@ -0,0 +1,10 @@
+export type RootStackParamList = {
+  Login: undefined;
+  HomeTabs: undefined;
+};
+
+export type HomeTabParamList = {
+  Home: undefined;
+  Scan: undefined;
+  RecordVoice: undefined;
+};
